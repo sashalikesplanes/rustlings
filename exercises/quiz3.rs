6f16e05733f3558086e840b97fda700e296273e4
Sasha Kiselev <sdkiselev1812@gmail.com>
2023-05-06 18:54:03 +0200

    quiz 3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,15 +14,14 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -50,7 +49,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
@@ -58,5 +57,11 @@ mod tests {
             report_card.print(),
             "Gary Plotter (11) - achieved a grade of A+"
         );
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_should_panic() {
+        panic!("Hello panic my old friend");
     }
 }
